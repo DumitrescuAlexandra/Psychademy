4896c614356c4615d3357104b7756a69fec8c592
Alexandra <xensandra3@gmail.com>
2021-08-20 19:38:03 +0300

    Manageable burger menu (redux installed, but not initialised)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import "typeface-lato";
-
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom';
+
+import "typeface-lato";
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
   <BrowserRouter>
-  <App />
-</BrowserRouter>,
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
