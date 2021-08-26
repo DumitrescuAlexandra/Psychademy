1213e07d5613a76b53ff17af19ed066231941e67
Alexandra <xensandra3@gmail.com>
2021-08-26 22:07:58 +0300

    flexed first page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import classes from "./App.module.css";
 import { Fragment } from "react";
 
 import MainNavigation from "./components/MainNavigation/MainNavigation";
@@ -8,9 +8,9 @@ function App() {
   return (
     <Fragment>
       <MainNavigation />
-      <div className={".hero"}>
-        <p className={".title"}> Adapt. Improvise. Overcome.</p>
-        <p className={".motto"}>
+      <div className={classes.hero}>
+        <p className={classes.title}> Adapt. Improvise. Overcome.</p>
+        <p className={classes.motto}>
           I can help you gain control over your cognitive resources and
           mastercraft your destiny.
         </p>
