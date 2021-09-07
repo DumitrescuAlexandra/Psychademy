45505737bc0dc360fa008361d1ab289f787958d9
Alexandra <xensandra3@gmail.com>
2021-09-07 22:18:37 +0300

    fullServices page]

--- src/screens/Services/ServicesCard.js
+++ src/screens/Services/ServicesCard.js
@@ -1,22 +1,63 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Services.module.css";
 
 function ServicesCard({ card, index, toggleCard }) {
   return (
-    <div
-      className={card.flip ? classes.card_flipped : classes.card}
-      key={index}
-      onClick={() => toggleCard(index)}
-    >
-      <div className={classes.card_text}> {card.text} </div>
-      <div>
-        <img
-          className={classes.serviceIcon}
-          src={card.imgSrc}
-          alt={card.altText}
-        ></img>
-      </div>
-    </div>
+    <Fragment>
+      {!card.flip && (
+        <div
+          className={classes.card}
+          key={index}
+          onClick={() => toggleCard(index)}
+        >
+          <div className={classes.card_text}> {card.text} </div>
+          <div>
+            <img
+              className={classes.serviceIcon}
+              src={card.imgSrc}
+              alt={card.altText}
+            ></img>
+          </div>
+        </div>
+      )}
+      {card.flip && (
+        <div
+          className={classes.card_flipped}
+          key={index}
+          onClick={() => toggleCard(index)}
+        >
+          <div className={classes.miniCard}>
+            <div className={classes.miniCard_text}> Duration </div>
+            <img
+              className={classes.miniIcon}
+              src="/Images/duration.svg"
+              alt="duration"
+            ></img>
+            <div className={classes.miniCard_specifics}>{card.duration}</div>
+          </div>
+
+          <div className={classes.miniCard}>
+            <div className={classes.miniCard_text}> Location </div>
+            <img
+              className={classes.miniIcon}
+              src="/Images/location.svg"
+              alt="location"
+            ></img>
+            <div className={classes.miniCard_specifics}>{card.location}</div>
+          </div>
+
+          <div className={classes.miniCard}>
+            <div className={classes.miniCard_text}> Price </div>
+            <img
+              className={classes.miniIcon}
+              src="/Images/price.svg"
+              alt="price"
+            ></img>
+            <div className={classes.miniCard_specifics}>{card.price}</div>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
