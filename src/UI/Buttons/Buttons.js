f8df7e326fc4ec0af3e0f6e6a27c6827f1a71b44
Alexandra <xensandra3@gmail.com>
2021-09-09 13:24:33 +0300

    toggle abt buttons semi-functional

--- src/UI/Buttons/Buttons.js
+++ src/UI/Buttons/Buttons.js
@@ -1,39 +1,82 @@
-import React, { useState } from "react";
-
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import classes from "./Buttons.module.css";
 
-const AboutButtons = () => {
-  const [active, setActive] = useState("true");
+function Buttons() {
+  const [active, setActive] = useState({
+    activeButton: null,
+    buttons: [
+      { name: "About Me", path: "/about/aboutMe" },
+      { name: "About CBT", path: "/about/aboutCBT" },
+    ],
+  });
 
-  const showAboutHandler = (event) => {
-    // event.preventDefault();
-    console.log("Activated!");
+  const toggleActiveHandler = (i) => {
+    setActive({ ...active, activeButton: active.buttons[i] });
+  };
+
+  const toggleActiveClass = (i) => {
+    if (active.buttons[i] === active.activeButton) {
+      return classes.btn_active;
+    } else {
+      return classes.btn_inactive;
+    }
   };
 
   return (
     <div className={classes.aboutBtn}>
-      <Link
-        className={classes.btn_aboutMe}
-        to="/about/aboutMe"
-        exact
-        onClick={showAboutHandler}
-      >
-        {" "}
-        About Me
-      </Link>
-
-      <Link
-        className={classes.btn_aboutCBT}
-        to="/about/aboutCBT"
-        exact
-        onClick={showAboutHandler}
-      >
-        {" "}
-        About CBT
-      </Link>
+      {active.buttons.map((el, i) => (
+        <Link
+          key={i}
+          className={toggleActiveClass(i)}
+          onclick={toggleActiveHandler}
+          to={active.buttons[i].path}
+        >
+          {active.buttons[i].name}
+        </Link>
+      ))}
     </div>
   );
-};
+}
 
-export default AboutButtons;
+export default Buttons;
+
+// import React, { useState } from "react";
+
+// import { Link } from "react-router-dom";
+// import classes from "./Buttons.module.css";
+
+// const AboutButtons = () => {
+//   // const [active, setActive] = useState("true");
+
+//   const showAboutHandler = (event) => {
+//     // event.preventDefault();
+//     console.log("Activated!");
+//   };
+
+//   return (
+//     <div className={classes.aboutBtn}>
+//       <Link
+//         className={classes.btn_aboutMe}
+//         to="/about/aboutMe"
+//         exact
+//         onClick={showAboutHandler}
+//       >
+//         {" "}
+//         About Me
+//       </Link>
+
+//       <Link
+//         className={classes.btn_aboutCBT}
+//         to="/about/aboutCBT"
+//         exact
+//         onClick={showAboutHandler}
+//       >
+//         {" "}
+//         About CBT
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default AboutButtons;
