fd7238f0ccfba963c34f171c7bad3f49de3e9a32
Alexandra <xensandra3@gmail.com>
2021-10-19 20:54:08 +0300

    Almost cute naivagtion for desktop resolution

--- src/components/MainNavigation/Navigation.js
+++ src/components/MainNavigation/Navigation.js
@@ -1,7 +1,76 @@
 import classes from "./MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  return <nav className={classes.navigation}></nav>;
+  return (
+    <div className={classes.navigation}>
+      <div className={classes.pcNav}>
+        <NavLink
+          to="/welcome"
+          activeStyle={{
+            borderBottom: "2px solid #0CC072",
+            paddingBottom: "0.5rem",
+            color: "#0CC072",
+          }}
+        >
+          Home
+        </NavLink>
+      </div>
+
+      <div className={classes.pcNav}>
+        <NavLink
+          to="/About"
+          activeStyle={{
+            borderBottom: "2px solid #0CC072",
+            paddingBottom: "0.5rem",
+            color: "#0CC072",
+          }}
+        >
+          About
+        </NavLink>
+      </div>
+
+      <div className={classes.pcNav}>
+        <NavLink
+          to="/Services"
+          activeStyle={{
+            borderBottom: "2px solid #0CC072",
+            paddingBottom: "0.5rem",
+            color: "#0CC072",
+          }}
+        >
+          Services
+        </NavLink>
+      </div>
+
+      <div className={classes.pcNav}>
+        <NavLink
+          to="/Contact"
+          activeStyle={{
+            borderBottom: "2px solid #0CC072",
+            paddingBottom: "0.5rem",
+            color: "#0CC072",
+          }}
+        >
+          Contact
+        </NavLink>
+      </div>
+
+      <div className={classes.pcNav}>
+        <NavLink
+          to="/PatientLogin"
+          activeStyle={{
+            border: "2px solid #0CC072",
+            paddingBottom: "0.5rem",
+            color: "#0CC072",
+          }}
+          className={classes.authNav}
+        >
+          Authentication
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
