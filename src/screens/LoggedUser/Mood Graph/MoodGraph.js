1343dffc7460f19896f91e39f113bae7bbca1b39
Alexandra <xensandra3@gmail.com>
2022-01-25 20:26:11 +0200

    rendering the moodcheck on graph Page

--- src/screens/LoggedUser/Mood Graph/MoodGraph.js
+++ src/screens/LoggedUser/Mood Graph/MoodGraph.js
@@ -1,28 +1,38 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import Plot from "react-plotly.js";
 import BackArrow from "../../../UI/Buttons/BackArrow";
 import classes from "./MoodGraph.module.css";
+import MoodCheck from "../../../screens/LoggedUser/WelcomeScreen/MoodCheck";
 
 function MoodGraph() {
   return (
     <Fragment>
       <BackArrow />
       <div className={classes.graphPage}>
-        <div className={classes.chartZone}>
-          <Plot
-            data={[
-              {
-                x: ["Mon", "Tue", "Wed", " Thu", "Fri", "Sat", "Sun"],
-                y: [1, 3, 2, 4, 5, 5, 4, 4],
-                type: "scatter",
-                mode: "lines+markers",
-                marker: { color: "#6EA5A5" },
-              },
-              // { type: "scatter", x: [1, 2, 3], y: [2, 5, 3] },
-            ]}
-            layout={{ width: 350, height: 300, title: "My graph" }}
-          />
+        <div className={classes.graphTitle}>
+          {" "}
+          <p> My evolution </p>{" "}
         </div>
+        <div className={classes.graphZone}>
+          <div className={classes.chartZone}>
+            <Plot
+              data={[
+                {
+                  x: ["Mon", "Tue", "Wed", " Thu", "Fri", "Sat", "Sun"],
+                  y: [1, 3, 2, 4, 5, 5, 4, 4],
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "#6EA5A5" },
+                },
+              ]}
+              layout={{ width: 350, height: 300, title: "My graph" }}
+            />
+          </div>
+        </div>
+        <Link to="/MoodGraph">
+          <MoodCheck />
+        </Link>
       </div>
     </Fragment>
   );
--- src/screens/LoggedUser/WelcomeScreen/MoodCheck.js
+++ src/screens/LoggedUser/WelcomeScreen/MoodCheck.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-modal";
 import { Link, useHistory } from "react-router-dom";
 import classes from "./MoodCheck.module.css";
 
@@ -14,54 +15,52 @@ function MoodCheck() {
     return "";
   };
 
-  const closeModal = (event) => {
-    event.preventDefault();
-    history.push("/UserPage");
-    console.log("EXIT modal");
-    return "";
-  };
-
-  const skipHandler = (event) => {
-    event.preventDefault();
-    history.push("/UserPage");
-  };
-
-  // CLOSE the modal INLINE ex: onclick={() => setModal(false)}
-
   return (
-    <div>
-      <div className={classes.background}></div>
-      <div className={classes.moodModal}>
+    <div className={classes.moodCheckPage}>
+      <Modal
+        isOpen={true}
+        className={classes.moodModalBck}
+        style={{
+          overlay: {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(28, 28, 28, 0.9)",
+          },
+        }}
+      >
         <img
           src="/Images/close.svg"
           alt=""
-          onClick={closeModal}
-          className={classes.close}
+          height="28px"
+          width="28px"
+          onClick={() => history.replace("/UserPage")}
+          className={classes.closeImg}
         ></img>
-        <p className={classes.moodTitle}> Welcome! </p>
-        <p className={classes.moodText}> How do you feel today? </p>
-        <div className={classes.moods}>
-          <img src="/Images/horrible.svg" alt="" onClick={toggleMood}></img>
-          <img src="/Images/sad.svg" alt="" onClick={toggleMood}></img>
-          <img src="/Images/meh.svg" alt="" onClick={toggleMood}></img>
-          <img src="/Images/happy.svg" alt="" onClick={toggleMood}></img>
-          <img src="/Images/wonderful.svg" alt="" onClick={toggleMood}></img>
+        <div className={classes.moodContainer}>
+          <p className={classes.moodTitle}> Welcome! </p>
+          <p className={classes.moodText}> How do you feel today? </p>
+          <div className={classes.moods}>
+            <img src="/Images/horrible.svg" alt="" onClick={toggleMood}></img>
+            <img src="/Images/sad.svg" alt="" onClick={toggleMood}></img>
+            <img src="/Images/meh.svg" alt="" onClick={toggleMood}></img>
+            <img src="/Images/happy.svg" alt="" onClick={toggleMood}></img>
+            <img src="/Images/wonderful.svg" alt="" onClick={toggleMood}></img>
+          </div>
+          <div className={classes.modalControls}>
+            <button onClick={moodHandler} className={classes.submitBtn}>
+              {" "}
+              Submit{" "}
+            </button>
+            <Link className={classes.skipBtn} to="/PatientLogin/UserPage">
+              <p>Skip for now</p>
+              <img src={"/Images/back.svg"} alt={"back"}></img>
+            </Link>
+          </div>
         </div>
-        <div className={classes.modalControls}>
-          <button onClick={moodHandler} className={classes.submitBtn}>
-            {" "}
-            Submit{" "}
-          </button>
-          <Link
-            className={classes.skipBtn}
-            onClick={skipHandler}
-            to="/PatientLogin/UserPage"
-          >
-            <p>Skip for now</p>
-            <img src={"/Images/back.svg"} alt={"back"}></img>
-          </Link>
-        </div>
-      </div>
+      </Modal>
     </div>
   );
 }
