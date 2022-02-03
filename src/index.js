e2e65ef9b1fa9719b748d12dba6c717d090a79e6
Alexandra <xensandra3@gmail.com>
2022-02-03 17:43:41 +0200

    removed "redux" from dependencies...

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import "typeface-lato";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import store from "./store/index";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-
-// To be developed
-
-const theManagerReducer = (state, action) => {};
-
-const store = createStore(theManagerReducer);
-
-export default store;
