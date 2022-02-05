b72c5ac6ac97f65c3de1fe4befaebed574dd0f15
Alexandra <xensandra3@gmail.com>
2022-02-05 19:06:29 +0200

    dropdown done correctly

--- src/screens/About/Dropdown/Dropdown.js
+++ src/screens/About/Dropdown/Dropdown.js
@@ -1,59 +1,76 @@
 import React, { useState } from "react";
 import classes from "./Dropdown.module.css";
-import FAQ from "./DropdownItem";
+
+import downArrow from "../../../Images/downArrow.svg";
+import upArrow from "../../../Images/upArrow.svg";
+
+const DROPDOWN_DATA = [
+  {
+    question: "What is CBT?",
+    answer:
+      "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
+  },
+  {
+    question: "What are the key concepts of CBT?",
+    answer:
+      "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
+  },
+  {
+    question: "How long does it take to complete CBT treatment",
+    answer:
+      "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
+  },
+  {
+    question: "Which patients benefit the most from CBT?",
+    answer:
+      "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
+  },
+  {
+    question: "What is the goal of psychological evaluation?",
+    answer:
+      "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
+  },
+];
 
 function Dropdown() {
-  const [faqs, setFaqs] = useState([
-    {
-      question: "What is CBT?",
-      answer:
-        "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
-      open: false,
-    },
-    {
-      question: "What are the key concepts of CBT?",
-      answer:
-        "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
-      open: false,
-    },
-    {
-      question: "How long does it take to complete CBT treatment",
-      answer:
-        "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
-      open: false,
-    },
-    {
-      question: "Which patients benefit the most from CBT?",
-      answer:
-        "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
-      open: false,
-    },
-    {
-      question: "What is the goal of psychological evaluation?",
-      answer:
-        "Cognitive behavioral therapy (CBT) is a type of psychotherapeutic treatment that helps people learn how to identify and change destructive or disturbing thought patterns that have a negative influence on behavior and emotions. The benefits of CBT have been championed by psychologists for decades. But now, new research suggests that CBT can not only change our thought patterns, it can literally rewire our brains!",
-      open: false,
-    },
-  ]);
+  const [clicked, setClicked] = useState(false);
 
-  const toggleFaq = (index) => {
-    setFaqs(
-      faqs.map((faq, i) => {
-        if (i === index) {
-          faq.open = !faq.open;
-        } else {
-          faq.open = false;
-        }
-        return faq;
-      })
-    );
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
   };
 
   return (
     <div className={classes.faqs}>
-      {faqs.map((faq, i) => {
-        return <FAQ faq={faq} index={i} key={i} toggleFaq={toggleFaq} />;
-      })}
+      <div className={classes.faq}>
+        {DROPDOWN_DATA.map((item, index) => {
+          return (
+            <>
+              <div
+                className={classes.faq_question}
+                onClick={() => toggle(index)}
+                key={index}
+              >
+                <p> {item.question} </p>
+                <span>
+                  {clicked === index ? (
+                    <img src={upArrow} alt="" />
+                  ) : (
+                    <img src={downArrow} alt="" />
+                  )}
+                </span>
+              </div>
+              {clicked === index ? (
+                <div className={classes.faq_answer}>
+                  <p>{item.answer}</p>
+                </div>
+              ) : null}
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/screens/About/Dropdown/DropdownItem.js
+++ src/screens/About/Dropdown/DropdownItem.js
@@ -1,23 +1,25 @@
-import React from "react";
-import classes from "./Dropdown.module.css";
+// import React from "react";
+// import classes from "./Dropdown.module.css";
 
-function FAQ({ faq, index, toggleFaq }) {
-  return (
-    <div
-      className={faq.open ? classes.faq_open : classes.faq}
-      key={index}
-      onClick={() => toggleFaq(index)}
-    >
-      <div className={classes.faq_question}>
-        {" "}
-        {faq.question}{" "}
-        {!faq.open && <img src={"/Images/downArrow.svg"} alt={"down"}></img>}
-        {faq.open && <img src={"/Images/upArrow.svg"} alt={"up"}></img>}
-      </div>
+// import downArrow from "../../../Images/downArrow.svg";
+// import upArrow from "../../../Images/upArrow.svg";
 
-      <div className={classes.faq_answer}> {faq.answer}</div>
-    </div>
-  );
-}
+// function FAQ({ faq, index, toggleFaq }) {
+//   return (
+//     <div
+//       className={faq.open ? classes.faq_open : classes.faq}
+//       key={index}
+//       onClick={() => toggleFaq(index)}
+//     >
+//       <div className={classes.faq_question}>
+//         {" "}
+//         {faq.question} {!faq.open && <img src={downArrow} alt={"down"}></img>}
+//         {faq.open && <img src={upArrow} alt={"up"}></img>}
+//       </div>
 
-export default FAQ;
+//       <div className={classes.faq_answer}> {faq.answer}</div>
+//     </div>
+//   );
+// }
+
+// export default FAQ;
